5ea7c23c193cfbc388165ffac0920d3ab4a21402
Lucas Dinesh <76265780+lucasdinesh@users.noreply.github.com>
2021-06-12 15:44:52 -0300

    Update Haversine.py

--- Haversine.py
+++ Haversine.py
@@ -1,25 +1,17 @@
 from math import radians, sin, cos, sqrt, asin
 
-
-def haversine(lat1, lon1, HashTable):
-    aux_table = []
-    for i in range(len(HashTable)):
-        if(HashTable[i] == ''):
-            pass
-        else:
-            lat2=HashTable[i][0]
-            lon2=HashTable[i][1]
-            R = 6372.8  # Earth radius in kilometers
-
-            dLat = radians(lat2 - lat1)
-            dLon = radians(lon2 - lon1)
-            lat1 = radians(lat1)
-            lat2 = radians(lat2)
-
-            a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
-            c = 2 * asin(sqrt(a))
-            aux_tuple= (i, R*c)
-            aux_table.append(aux_tuple)
-
-
-    return aux_table #devolve um lista de tuplas contendo (codigo, distancia entre ponto e usuario)
+def haversine(lat1, lon1, lat2, lon2, codigo):
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    lat2 = radians(lat2)
+    lat1 = radians(lat1)
+    
+    R = 6372.8  # Earth radius in kilometers
+    
+    a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    
+    print(R, a, c)
+    aux_tuple= (codigo, R*c)
+    
+    return aux_tuple
