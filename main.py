2e0f0ca2bb87e2b9dfe57363c3943bf8e5d224f5
Lucas Dinesh <76265780+lucasdinesh@users.noreply.github.com>
2021-06-12 09:16:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from reader_PontosdeTaxi import reader_taxi
 from menu import menu
 
 
+
 HashTable = ['' for _ in range(739)]    #[latitude, longitude, logradouro, numero, telefone]
 reader_taxi(HashTable)
-menu(HashTable)
+menu(HashTable) #design pattern proxy
