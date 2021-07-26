c8860bd612e81d731e5473c062fd2df0ed2f0937
Lucas Dinesh <lucasdinesh@gmail.com>
2021-07-26 11:05:27 -0300

    arroz com feijao

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from menu import menu
 HashTable = ['' for _ in range(739)]    #[latitude, longitude, logradouro, numero, telefone]
 reader_taxi(HashTable)
 menu(HashTable) #design pattern proxy
+
+print("eu amo arroz com feijao, certamente comeria todo dia")
+
